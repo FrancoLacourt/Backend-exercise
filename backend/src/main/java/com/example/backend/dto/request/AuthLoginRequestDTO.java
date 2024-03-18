888218ef2226bf687d1d09c0928d0e3163d64fde
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-03-18 18:58:59 -0300

    Create AuthLoginRequestDTO.java

---
+++ backend/src/main/java/com/example/backend/dto/request/AuthLoginRequestDTO.java
@@ -0,0 +1,12 @@
+package com.example.backend.dto.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AuthLoginRequestDTO {
+
+    private String email;
+    private String password;
+}
