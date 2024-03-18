c558591c53e8777ea51a7b078cafe3029ee753ad
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-03-18 16:23:36 -0300

    Changed project's title

--- backend/src/test/java/com/example/backend/NoteTests/TagMapperTest.java
+++ backend/src/test/java/com/example/backend/NoteTests/TagMapperTest.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class TagMapperTest {
