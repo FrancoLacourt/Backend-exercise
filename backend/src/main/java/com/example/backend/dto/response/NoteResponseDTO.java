a78fa60b5c4437ec627afa5d602f1cca40f10b95
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-03-25 14:30:51 -0300

    Update NoteResponseDTO.java

--- backend/src/main/java/com/example/backend/dto/response/NoteResponseDTO.java
+++ backend/src/main/java/com/example/backend/dto/response/NoteResponseDTO.java
@@ -1,4 +1,17 @@
 package com.example.backend.dto.response;
 
+import com.example.backend.entity.Tag;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class NoteResponseDTO {
+
+    private Long id_note;
+    private String title;
+    private String description;
+    private boolean enabled = true;
+    private List<Tag> tags;
+
 }
