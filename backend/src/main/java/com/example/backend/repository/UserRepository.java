17f6d806d075310b1919951d13b9b1b91e4a7185
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-04-17 19:13:20 -0300

    Update UserRepository.java

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.example.backend.repository;
 import com.example.backend.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
@@ -11,5 +10,4 @@ public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
     Optional<UserEntity> findUserByEmail(String email);
     boolean existsByEmail(String email);
-
 }
