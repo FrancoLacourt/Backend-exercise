3f96e5fd73e6adc14bf5c16109c2bc941f4f6e83
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-04-17 19:14:31 -0300

    Update NoteMapperTest.java

--- backend/src/test/java/com/example/backend/Tests/NoteTests/NoteMapperTest.java
+++ backend/src/test/java/com/example/backend/Tests/NoteTests/NoteMapperTest.java
@@ -1,6 +1,5 @@
 package com.example.backend.Tests.NoteTests;
 
-
 import com.example.backend.dto.request.NoteRequestDTO;
 import com.example.backend.dto.response.NoteResponseDTO;
 import com.example.backend.entity.Note;
@@ -10,10 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
