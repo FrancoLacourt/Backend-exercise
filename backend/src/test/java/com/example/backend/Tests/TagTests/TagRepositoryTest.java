9abeceb8c7d7fbdd05ea70a58d9a227df36247a9
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-04-17 19:14:20 -0300

    Update TagRepositoryTest.java

--- backend/src/test/java/com/example/backend/Tests/TagTests/TagRepositoryTest.java
+++ backend/src/test/java/com/example/backend/Tests/TagTests/TagRepositoryTest.java
@@ -7,11 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -108,5 +106,4 @@ public class TagRepositoryTest {
         assertTrue(foundTag.isPresent());
         assertEquals(foundTag.get().getTagName(), "Music");
     }
-    
 }
