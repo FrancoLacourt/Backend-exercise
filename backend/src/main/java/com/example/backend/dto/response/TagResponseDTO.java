fcbc3981b991603f8dfed5175867421a66815c69
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-04-17 19:14:56 -0300

    Update TagResponseDTO.java

--- backend/src/main/java/com/example/backend/dto/response/TagResponseDTO.java
+++ backend/src/main/java/com/example/backend/dto/response/TagResponseDTO.java
@@ -3,7 +3,6 @@ package com.example.backend.dto.response;
 import com.example.backend.entity.Note;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-
 import java.util.List;
 
 @Data
@@ -14,5 +13,4 @@ public class TagResponseDTO {
 
     @JsonIgnore
     private List<Note> notes;
-
 }
