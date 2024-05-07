b77bb4727356e43739408ac0a276e613933cedc1
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-05-07 16:07:38 -0300

    Updated Dockerfile

--- backend/target/generated-sources/annotations/com/example/backend/mapper/TagMapperImpl.java
+++ backend/target/generated-sources/annotations/com/example/backend/mapper/TagMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-05-06T18:50:16-0300",
+    date = "2024-05-07T15:42:28-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.9 (Eclipse Adoptium)"
 )
 @Component
